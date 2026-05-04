1434c7d95efc0f0e73fe807397eaa8c90f366c98
Chris <beingchris3344@gmail.com>
2026-05-05 00:22:57 +0300

    BASE_URL =

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -11,7 +11,7 @@ if (hostname === "localhost") {
 } 
 else {
   // Production (deployed frontend) → backend URL
-  BASE_URL = "https://zuca-backend-iw9p.onrender.com/";
+  BASE_URL = "https://zuca-backend-iw9p.onrender.com";
 }
 
 // Create a public API instance (NO authentication)
